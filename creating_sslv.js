6a23cec37043cc599f9c41735803daa6b7fc409b
rihrds <151781904+rihrds@users.noreply.github.com>
2024-03-28 12:16:48 +0200

    Add files via upload

--- creating_sslv.js
+++ creating_sslv.js
@@ -21,9 +21,22 @@ function process_data_sslv(data){
     var items = Array.from(xml_doc.getElementsByTagName("item"));
     console.log(items);
 
+    data = [];
+
     items.forEach(item => {
-        console.log(item.textContent);
+        return_data = {};
+        nodes = item.childNodes;
+        //console.log(nodes[1].textContent);
+        return_data["listing"] = nodes[3].textContent; //sludinajuma adrese
+        return_data["date"] = Date.parse(nodes[5].textContent); //publicesanas datums
+        arr = nodes[7].textContent.split("<br")
+        arr.forEach( (e, i) => {arr[i] = e.replace("/>", "")})
+        arr = arr.filter(n => n); //parejais info
+        console.log(arr)
+        data.push(return_data);
     })
+
+    //console.log(data);
 }
 
 async function do_all(){
